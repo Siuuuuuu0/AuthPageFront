e760c948bd71ef7d1607c7131adb82ab1226df0f
Vadimk <vadimkotkin1855@gmail.com>
2024-07-29 00:43:06 +0200

    env

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import { store } from './app/store'
 import { Provider } from 'react-redux'
 
 const client = process.env.REACT_APP_GOOGLE_CLIENT_ID
-console.log(client)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
